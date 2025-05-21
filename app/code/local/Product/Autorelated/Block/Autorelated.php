72972fbc2ee744191ceb3da4b07f0599b987228c
LuizSantos1 <grlsantos@hotmail.com>
2025-05-20 23:02:42 -0300

    Update Autorelated.php
    making it compatible with openmage v20.13.0

--- app/code/local/Product/Autorelated/Block/Autorelated.php
+++ app/code/local/Product/Autorelated/Block/Autorelated.php
@@ -2,18 +2,20 @@
 
 class Product_Autorelated_Block_Autorelated extends Mage_Catalog_Block_Product_View
 {
-
     /**
-     * @return null
+     * Retrieve a collection of related products from the same category
+     *
+     * @return Mage_Catalog_Model_Resource_Product_Collection|array
      */
     public function getRelatedCollection()
     {
-        $collection = array();
+        $collection = [];
+
         /** @var Product_Autorelated_Helper_Data $helper */
         $helper = $this->helper('product_autorelated');
 
         $category = $this->_getProductCategory();
-        if (empty($category)) {
+        if (!$category) {
             return $collection;
         }
 
@@ -21,9 +23,8 @@ class Product_Autorelated_Block_Autorelated extends Mage_Catalog_Block_Product_V
         $collection = Mage::getResourceModel('catalog/product_collection')
             ->addAttributeToSelect('*')
             ->setStoreId($storeId)
-            ->addFieldToFilter('entity_id', array('nin' => array($this->getProduct()->getId()))) // exclude currently viewed product from collection
+            ->addFieldToFilter('entity_id', ['nin' => [$this->getProduct()->getId()]])
             ->addCategoryFilter($category);
-
 
         Mage::getSingleton('catalog/product_status')->addVisibleFilterToCollection($collection);
         Mage::getSingleton('catalog/product_visibility')->addVisibleInCatalogFilterToCollection($collection);
@@ -36,19 +37,26 @@ class Product_Autorelated_Block_Autorelated extends Mage_Catalog_Block_Product_V
     }
 
     /**
-     * @return mixed
+     * Get the last (most specific) active category assigned to the product
+     *
+     * @return Mage_Catalog_Model_Category|null
      */
     protected function _getProductCategory()
     {
         $product = $this->getProduct();
         $storeId = Mage::app()->getStore()->getId();
 
+        $categoryIds = $product->getCategoryIds();
+        if (empty($categoryIds)) {
+            return null;
+        }
+
         $categories = Mage::getModel('catalog/category')->getCollection()
             ->setStoreId($storeId)
             ->addAttributeToFilter('is_active', 1)
-            ->addFieldToFilter('entity_id', array('in' => $product->getCategoryIds()))
+            ->addFieldToFilter('entity_id', ['in' => $categoryIds])
             ->addAttributeToSelect('*');
 
-        return $categories->getLastItem();
+        return $categories->getLastItem() ?: null;
     }
 }
