efa901329d72486b1bc3f99f97ae490604d802ee
LuizSantos1 <grlsantos@hotmail.com>
2025-05-20 23:04:57 -0300

    Update Data.php

--- app/code/local/Product/Autorelated/Helper/Data.php
+++ app/code/local/Product/Autorelated/Helper/Data.php
@@ -1,19 +1,26 @@
 <?php
+
 class Product_Autorelated_Helper_Data extends Mage_Core_Helper_Abstract
 {
     /**
-     * Default collection size
+     * Default number of products to display
      */
     const PRODUCTS_COLLECTION_COUNT = 10;
 
     /**
-     * Configuration path
+     * XML config path for collection count
      */
     const XML_PATH_COLLECTION_COUNT = 'catalog/autorelated_group/count';
 
+    /**
+     * Get the number of products to display in the related collection
+     *
+     * @return int
+     */
     public function getCollectionSize()
     {
-        $config = (int)Mage::getStoreConfig(self::XML_PATH_COLLECTION_COUNT, Mage::app()->getStore());
-        return ($config) ? $config : self::PRODUCTS_COLLECTION_COUNT;
+        $config = (int) Mage::getStoreConfig(self::XML_PATH_COLLECTION_COUNT, Mage::app()->getStore());
+
+        return ($config > 0) ? $config : self::PRODUCTS_COLLECTION_COUNT;
     }
 }
